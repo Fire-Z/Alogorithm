efb9a5479591874f1a53429503ce352c80a44e17
Fire-Z <1747834798@qq.com>
2022-05-23 15:34:08 +0800

    合并K个有序列表

---
+++ C++/leetcode/linkedList/l2.cpp
@@ -0,0 +1,27 @@
+/*
+No.23 level.hard Name.合并K个有序列表
+*/
+
+#include <iostream>
+#include <vector>
+#include "structs.h"
+using namespace std;
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        // 标记每个链表的现处位置
+        vector<int> locators(lists.size(), 0);
+        ListNode* rtn;
+
+
+
+        return rtn;
+        
+    }
+};
+
+int main()
+{
+    
+}
